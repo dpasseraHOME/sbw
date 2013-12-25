cc44632e3c26a29b16ef50c0db8350c1598a4a0f
dpasseraHOME <dante.francesco.passera@gmail.com>
2013-12-25 18:38:20 -0500

    rudimentary pencil and eraser

--- js/main.js
+++ js/main.js
@@ -1,18 +1,49 @@
 $(document).ready(function() {
 	console.log('!! ready');
 
+	drawing.initPixelArray();
 	drawing.initCanvas();
+	drawing.initTools();
 });
 
 var drawing = {
 
-	mContext : null,
-	mIsLineStarted: false,
-	mIsMouseDown: false,
-	mCountPixelsX: 32,
-	mCountPixelsY: 32,
+	PENCIL : 'pencil',
+	ERASER : 'eraser',
 
-	initCanvas :function() {
+	mContext : null,
+	mIsLineStarted : false,
+	mIsMouseDown : false,
+	//* Number of sprite 'pixels' wide and high.
+	mCountPixelsX : 32,
+	mCountPixelsY : 32,
+	//* Number of screen pixels per sprite 'pixel'.
+	mPixelSize : 10,
+
+	//* 3D array representing the pixel grid.
+	mPxArr : [],
+	//* Hex value of transparent (erased) color. Hex transparencies range 00 - FF.
+	mTransColor : '(0, 0, 0, 0)',
+	//* Hex value of WIP pencil color.
+	mPencilColor : '(0, 0, 0, 1)',
+	//* Hex value of currently selected color.
+	mSelColor : null,
+
+	/**
+	*	Initialize grid with transparent pixels at all locations.
+	**/
+	initPixelArray : function() {
+		var i, j;
+
+		for(i=0; i<drawing.mCountPixelsX; i++) {
+			drawing.mPxArr[i] = [];
+			for(j=0; j<drawing.mCountPixelsY; j++) {
+				drawing.mPxArr[i][j] = drawing.mTransColor;
+			}
+		}
+	},
+
+	initCanvas : function() {
 		console.log('# initCanvas');
 
 		var canvas = document.getElementById('canvas_draw');
@@ -21,6 +52,12 @@ var drawing = {
 			console.log('!! Error: failed to getContext');
 			return;
 		}
+
+		// set sprite pixel size based on number of sprite pixels and width of canvas
+		drawing.mPixelSize = $('#canvas_draw').width() / drawing.mCountPixelsX;
+
+		// set initial selected color
+		drawing.onSelectColor(drawing.mPencilColor);
 
 		$('#canvas_draw').mousemove(function(e) {
 			drawing.onMouseMoveOverCanvasDraw(e);
@@ -33,24 +70,54 @@ var drawing = {
 		});
 	},
 
-	onMouseMoveOverCanvasDraw :function(e) {
+	initTools : function() {
+		$('#a_tool_pencil').click({toolType: drawing.PENCIL}, drawing.onToolClick);
+		$('#a_tool_eraser').click({toolType: drawing.ERASER}, drawing.onToolClick);
+	},
+
+	onMouseMoveOverCanvasDraw : function(e) {
 		// console.log(e.pageX+', '+e.pageY);
 
 		var x = e.pageX - $(e.target).offset().left;
-		var y = e.pageY - $(e.target).offset().top; 
+		var y = e.pageY - $(e.target).offset().top;
+
+		var pxX, pxY;
 
 		if(drawing.mIsMouseDown) {
-			console.log(Math.floor(x/10)+', '+Math.floor(y/10));
-		}
+			// changes value in pixel color array
+			pxX = Math.floor(x/10);
+			pxY = Math.floor(y/10);
+			// console.log(pxX+', '+pxY);
+			drawing.mPxArr[pxX][pxY] = drawing.mSelColor;
 
-		/*if(!drawing.mIsLineStarted) {
-			drawing.mContext.beginPath();
-			drawing.mContext.moveTo(x, y);
-			drawing.mIsLineStarted = true;
-		} else {
-			drawing.mContext.lineTo(x, y);
-			drawing.mContext.stroke();
-		}*/
+			// draw sprite pixel
+			if(drawing.mSelColor == drawing.mTransColor) {
+				// is erasing
+				drawing.mContext.clearRect(pxX*drawing.mPixelSize, pxY*drawing.mPixelSize, drawing.mPixelSize, drawing.mPixelSize);
+			} else {
+				// is drawing
+				drawing.mContext.fillRect(pxX*drawing.mPixelSize, pxY*drawing.mPixelSize, drawing.mPixelSize, drawing.mPixelSize);
+			}
+		}
+	},
+
+	onToolClick : function(e) {
+		console.log('# onToolClick: '+e.data.toolType);
+
+		switch(e.data.toolType) {
+			case drawing.PENCIL:
+				drawing.onSelectColor(drawing.mPencilColor);
+				break;
+			case drawing.ERASER:
+				drawing.onSelectColor(drawing.mTransColor);
+				break;
+		};
+	},
+
+	onSelectColor : function(rgbaStr) {
+		console.log('# onSelectColor: '+rgbaStr);
+		drawing.mSelColor = rgbaStr;
+		drawing.mContext.fillStyle = 'rgba'+rgbaStr;
 	}
 
 };
